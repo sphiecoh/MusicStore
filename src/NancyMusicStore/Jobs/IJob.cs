338a00ad1fea3076d60c9537972ebc9ef8af288c
Sifiso Shezi <sfiso.dlaba@gmail.com>
2018-01-23 13:27:14 +0200

    WIP
    WIP

---
+++ src/NancyMusicStore/Jobs/IJob.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NancyMusicStore.Jobs
+{
+    interface IJob
+    {
+        void Run();
+        string Name { get; set; }
+        string Cron { get; set; }
+        JobType JobType { get; set; }
+
+    }
+   public  enum JobType
+    {
+        Reccuring,
+        OnceOff
+    }
+}
